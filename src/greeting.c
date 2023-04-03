f6fc7b694c8116eb07ea4f386f54b35290a15d51
dainank <whelbe@outlook.com>
2023-04-03 20:23:02 +0100

    greeting v4

--- src/greeting.c
+++ src/greeting.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <time.h>
- 
+
 int main(int argc, char *argv[])
 {
     time_t now;
     struct tm *clock;
-    int hour;
- 
+    char time_string[64];
+
     time(&now);
     clock = localtime(&now);
-    hour = clock->tm_hour;
- 
-    printf("Good ");
-    if( hour < 12 )
-        printf("morning");
-    else if( hour < 17 )
-        printf("afternoon");
-    else
-        printf("evening");
- 
-    if( argc>1 )
-        printf(", %s",argv[1]);
- 
-    putchar('\n');
- 
-    return(0);
+
+    strftime(time_string, 64, "Today is %A, %B %d, %Y%nIt is %r%n", clock);
+
+    printf("Greetings");
+    if (argc > 1)
+        printf(", %s", argv[1]);
+    printf("!\n%s", time_string);
+
+    return (0);
 }
